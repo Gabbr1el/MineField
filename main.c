cd324239c1f76e502fff6c048c4d6aba019c4072
Gabriel <felipegabrielsr03@gmail.com>
2025-09-19 22:50:38 -0300

    Condicionais para inicio do jogo

--- main.c
+++ main.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-   const BOMBA = -1, VAZIO = 0;
-
+   int tamanho, ganhou = 0, bombas;
    
+   while(ganhou == 0){
+      printf("Escreva o tamanho do campo min 5x5 max 100x100: ");
+      scanf("%d", &tamanho);
+      if(tamanho>=5 && tamanho<=100){
+         system("cls");
+      }
+      else{
+         system("cls");
+         printf("O tamanho e invalido!\n");
+         continue;
+      }
+      int mat[tamanho][tamanho];
+      printf("Quantas bombas vai ter? ");
+      scanf("%d",&bombas);
+      if(bombas<tamanho){
+         system("cls");
+         break;
+      }
+      else{
+         system("cls");
+         printf("A quantidade de bombas e invalida!\n");
+         continue;
+      }
 
-
+   }
 }
---
+++ teste.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main(){
+   printf("Escreva o tamanho do campo min 5x5 max 100x100:");
+}
